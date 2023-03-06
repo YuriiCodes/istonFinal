438e0c8a64a91bcaa3868f682af7217142163c8d
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-03-06 18:25:52 +0200

    Added styles for the embedded section.

--- src/components/EmbededAlbumSlice/EmbeddedAlbumSlice.tsx
+++ src/components/EmbededAlbumSlice/EmbeddedAlbumSlice.tsx
@@ -1,20 +1,26 @@
 import {FrontpageSlicesEmbed_Album_Section} from "../../prismic-types";
 import s from "./EmbeddedAlbumSlice.module.scss";
-import {Spotify} from "react-spotify-embed";
+
 type EmbeddedAlbumSliceProps = {
   slice: FrontpageSlicesEmbed_Album_Section;
 }
 
 
 export const EmbeddedAlbumSlice = ({slice}: EmbeddedAlbumSliceProps) => {
+  // the playlist url is  in format https://open.spotify.com/playlist/2hhBY8O3h8hL5MzTQmlWZs?si=749b142ff8254b6b.
+  // Get  the album id from the url:
+  const albumId = slice.variation?.primary?.playlisturl?.split("/")[4].split("?")[0];
   return (
-    <div>
-      <iframe className={s.embeddedFrame}
-              src="https://open.spotify.com/embed/playlist/2hhBY8O3h8hL5MzTQmlWZs?utm_source=generator&theme=0"
-              width="100%" height="352" frameBorder="0" allowFullScreen={true}
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              loading="lazy">
-      </iframe>
+    <div className={s.container}>
+      <h2>{slice.variation?.primary?.title[0]?.text}</h2>
+      <div className={s.embeddedFrameContainer}>
+        <iframe className={s.embeddedFrameContainer__embeddedFrame}
+                src={`https://open.spotify.com/embed/playlist/${albumId}?utm_source=generator&theme=0`}
+                width="100%" height="352" frameBorder="0" allowFullScreen={true}
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy">
+        </iframe>
+      </div>
     </div>
   )
 }
